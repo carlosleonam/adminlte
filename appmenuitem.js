dcdf5601a241c76322345c55c220523ca2fd2ea5
carlosleonam <carlosleonam@users.noreply.github.com>
2018-06-08 06:21:27 -0300

    Maintain sidebar menu open on reload page
    Maintain sidebar menu open on reload page. Adapted to template "theme3" in Adianti Framework (https://www.adianti.com.br/framework)

---
+++ appmenuitem.js
@@ -0,0 +1,26 @@
+/*
+* @Author: SisSoftwares WEB (Sistemas PHP)
+* @Date:   2018-06-07 13:12:35
+* @Last Modified by:   Usuario
+* @Last Modified time: 2018-06-08 05:56:20
+*/
+
+/*menu handler*/
+$(function(){
+
+  function getPart(str, separator, part) {
+    return str.split(separator)[part];
+  }
+
+  var url = window.location +''; // Add "+''" to get location like string
+  var urlClass = getSecondPart(url, '?', 1);
+  $('li a[href="'+ 'index.php?' + urlClass +'"]').closest('ul').closest('li').toggleClass('active');
+  $('li a[href="'+ 'index.php?' + urlClass +'"]').closest('ul').addClass('menu-open');
+
+  // Toggle check-icon
+  var url = window.location;
+  $('ul.sidebar-menu a').filter(function() {
+     return this.href == url;
+  }).parent().find('i').toggleClass('fa-circle-o fa-check-circle');
+
+});
